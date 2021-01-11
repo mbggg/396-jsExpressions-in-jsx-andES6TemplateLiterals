4743ee1654df488542b188bcd56f99b78d78b76a
Melissa <34263527+mbggg@users.noreply.github.com>
2021-01-11 13:25:27 -0500

    elaborated the comments

--- src/index.js
+++ src/index.js
@@ -6,5 +6,6 @@ import ReactDOM from "react-dom";
 // creating js constant and storing its data
 const name = "Mel";
 
-// in order to plug it in the HTML, write the constant in curly brackets
+// in order to plug the js constant in the HTML inside the JSX file,
+// write the constant in curly brackets, wrap the javascript inside curly braces
 ReactDOM.render(<h1>Hello {name}!</h1>, document.getElementById("root"));
