dd2e647d302e5d474ee3b41ebbbc517d588a4c49
Melissa <34263527+mbggg@users.noreply.github.com>
2021-01-11 13:24:14 -0500

    insert js constant inside jsx's html using curly brackets JavaScript code inside the HTML inside the JavaScript file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+// 396. JavaScript Expressions in JSX & E@6 Template Literals
+
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+// creating js constant and storing its data
+const name = "Mel";
+
+// in order to plug it in the HTML, write the constant in curly brackets
+ReactDOM.render(<h1>Hello {name}!</h1>, document.getElementById("root"));
